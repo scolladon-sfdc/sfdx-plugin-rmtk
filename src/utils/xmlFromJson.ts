cf6b9feb0cbf96826386983bb64dded4f53692aa
Sébastien Colladon <colladonsebastien@gmail.com>
2020-02-20 11:57:24 +0100

    :bug: fix empty file detection

--- src/utils/xmlFromJson.ts
+++ src/utils/xmlFromJson.ts
@@ -5,7 +5,9 @@ const end = { pretty: true, indent: '    ', newline: '\n' }
 
 const packageBuilder = (packageContent: String): String => {
     const pkg = {};
-    packageContent['Package']['types'].reduce((r, e) => pkg[e.name[0]] = [...new Set((pkg[e.name[0]] || []).concat(e.members))], pkg)
+    if (packageContent['Package']['types'] !== undefined) {
+        packageContent['Package']['types'].reduce((r, e) => pkg[e.name[0]] = [...new Set((pkg[e.name[0]] || []).concat(e.members))], pkg)
+    }
     const xml = xmlbuilder.create('Package')
         .att('xmlns', 'http://soap.sforce.com/2006/04/metadata')
         .dec('1.0', 'UTF-8');
@@ -32,11 +34,14 @@ const customLabelBuilder = (clContent: String): String => {
     const xml = xmlbuilder.create('CustomLabels')
         .att('xmlns', 'http://soap.sforce.com/2006/04/metadata')
         .dec('1.0', 'UTF-8');
-    clContent['CustomLabels']['labels'].forEach(label => {
-        const labelNode = xml.ele('labels');
-        properties.filter(prop => label.hasOwnProperty(prop)).forEach(prop =>
-            labelNode.ele(prop, label[prop][0]));
-    });
+    if (clContent['CustomLabels']['labels'] !== undefined) {
+        clContent['CustomLabels']['labels'].forEach(label => {
+            const labelNode = xml.ele('labels');
+            properties.filter(prop => label.hasOwnProperty(prop)).forEach(prop =>
+                labelNode.ele(prop, label[prop][0]));
+        });
+    }
+
     return xml.end(end);
 }
 
